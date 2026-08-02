39bd65071f7cc6cb37b6fe543c6aa3a4762cd346
Edoc-ps <eduardo.octavio@secondmind.com.br>
2026-08-02 20:48:53 -0300

    feat: remove dueDate column from EssayPrompt table

---
+++ prisma/migrations/20260802234654_remove_essay_prompt_due_date/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `dueDate` on the `EssayPrompt` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "EssayPrompt" DROP COLUMN "dueDate";
